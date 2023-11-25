028e14f113fe2d1adba3d3ffc8b99e118b0cf2fc
143WaysToLooseUrSelf <blaxdeww@gmail.com>
2023-11-25 23:46:54 +0300

    2

---
+++ PZ_5_11/PZ_5_1.py
@@ -0,0 +1,11 @@
+import random
+
+def chars():
+    char = ''
+    for i in range(40):
+        char += chr(random.randint(75, 120))
+    print(f"Cтрока из 40 символов: {char}")
+    print(f"Количество символов: {len(char)}")
+
+
+chars()
