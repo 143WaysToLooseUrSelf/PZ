8638c7fdfa294fddf1e17f15905b711ab7cfa4cc
student <vladtankist577@gmail.com>
2024-03-09 10:39:04 +0300

    123

--- pz12/12_1.py
+++ pz12/12_1.py
@@ -1,4 +1,4 @@
-# 1.В последовательности на n целых чисел найти и вывести:
+# В последовательности на n целых чисел найти и вывести:
 # 1. максимальный среди положительных
 # 2. минимальный среди отрицательных
 # 3. произведение элементов
@@ -6,12 +6,11 @@
 import random
 from functools import reduce
 
-n = 10
+n = 5
 
 num = [random.randint(-10, 10) for i in range(n)]
 positive_max = max(filter(lambda x: x > 0, num))
 negative_max = min(filter(lambda x: x < 0, num))
-
 proizvedenie = reduce(lambda x, y: x * y, num)
 
 print(f"Последовательность чисел: {num}")
