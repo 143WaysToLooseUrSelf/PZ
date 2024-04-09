ae80eade8a65dd8b081d774cd497c7e01ba2082b
143WaysToLooseUrSelf <blaxdeww@gmail.com>
2024-04-09 11:33:26 +0300

    13pz

--- pz13/13_1.py
+++ pz13/13_1.py
@@ -1,17 +1,10 @@
+# В матрице найти минимальный и максимальные элементы.
+
 import random
-from functools import reduce
 
-N = int(input("Введите размер матрицы: "))
-matrix = [[random.randint(-10, 10) for el in range(N)] for row in range(N)]
-print(f"Исходная матрица: {matrix}")
+N = int(input('Введите размер матрицы: '))
+matrix = [[random.randint(0, 20) for i in range(N)] for i in range(N)]
 
-for row in matrix:
-    print(row)
-
-cols = [reduce(lambda x, y: x + y, [row[i] for row in matrix]) for i in range(N)]
-print(f"Сумма элементов каждого столбца: {cols}")
-
-matrix[1] = cols
-print(f"Матрица после замены 2й строки на сумму столбцов: {matrix}")
-for row in matrix:
-    print(row)
+print(f'Исходная матрица: {matrix}')
+print('Минимальный элемент: ', min(min(matrix)))
+print('Максимальный элемент: ', max(max(matrix)))
