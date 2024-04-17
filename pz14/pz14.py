9acedf62e6007d181b642838df74975891ae03e3
Anton2442 <tutuponchick@gmail.com>
2024-04-17 12:43:14 +0300

    14pz

--- pz14/pz14.py
+++ pz14/pz14.py
@@ -1,3 +1,13 @@
+# Из исходного текстового файла (expansion.txt) выбрать имена файлов, соответствующие типам:
+# .xls, .xml, .html, .css, .py. Посчитать количество полученных элементов
+
 import re
 
-with open('Dostoevskiy', )
+pat = r'([^\s]+\.(xls|xml|html|css|py))'
+with open("expansion.txt", "r", encoding="utf-8") as file:
+    matches = re.findall(pat, file.read())
+
+for match in matches:
+    print(match)
+
+print("Общее количество файлов: ", len(matches))
